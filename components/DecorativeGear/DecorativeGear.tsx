8ed0e39944d9d75d5cb9f7fc1ed46d74ac25e26d
Trevor Cash <trevorcash.cs@gmail.com>
2026-01-06 18:48:59 -0500

    chore: removes unused components

--- components/DecorativeGear/DecorativeGear.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const DecorativeGear: React.FC<{ className?: string; size?: number }> = ({ className = '', size = 64 }) => (
-  <svg className={className} width={size} height={size} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" aria-hidden>
-    <circle cx="12" cy="12" r="10" stroke="#ccc" />
-  </svg>
-);
-
-export default DecorativeGear;
