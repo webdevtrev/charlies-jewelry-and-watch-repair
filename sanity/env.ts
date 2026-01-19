d765f4418c99f34debadfb338489eeac2bb9e518
Trevor Cash <trevorcash.cs@gmail.com>
2026-01-18 21:39:21 -0500

    fix: removes string of env variable names

--- sanity/env.ts
+++ sanity/env.ts
@@ -1,20 +1,20 @@
 export const apiVersion =
-  process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2026-01-18'
+  process.env.NEXT_PUBLIC_SANITY_API_VERSION || "2026-01-18";
 
 export const dataset = assertValue(
   process.env.NEXT_PUBLIC_SANITY_DATASET,
-  'Missing environment variable: NEXT_PUBLIC_SANITY_DATASET'
-)
+  "Missing environment variable for what data set to use"
+);
 
 export const projectId = assertValue(
   process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-  'Missing environment variable: NEXT_PUBLIC_SANITY_PROJECT_ID'
-)
+  "Missing environment variable for what project ID to use"
+);
 
 function assertValue<T>(v: T | undefined, errorMessage: string): T {
   if (v === undefined) {
-    throw new Error(errorMessage)
+    throw new Error(errorMessage);
   }
 
-  return v
+  return v;
 }
